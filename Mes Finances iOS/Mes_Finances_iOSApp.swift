ed85bd6c1b2d59c93fb0a44b0fd01297eb871189
Jérémy Rocher <rocherje@outlook.com>
2026-04-23 23:36:53 +0200

    Initial Commit

---
+++ Mes Finances iOS/Mes_Finances_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mes_Finances_iOSApp.swift
+//  Mes Finances iOS
+//
+//  Created by JEREMY on 23/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Mes_Finances_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
